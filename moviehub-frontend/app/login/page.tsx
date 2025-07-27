9236e9baa48df3499cd7c0bba3d44dff8143b17a
Yohan-beep <y.k.arikupuram@student.utwente.nl>
2025-07-27 17:11:39 +0200

    Login-page - email/password inputs and LOGO Design implementation

--- moviehub-frontend/app/login/page.tsx
+++ moviehub-frontend/app/login/page.tsx
@@ -29,7 +29,8 @@ export default function loginPage() {
             bg: "bg-gradient-to-br from-rose-500 via-purple-900 to-gray-900"
         }
     ];
-    const[currentPosterIndex, setPosterIndex] = useState(0);// 0 here initializes currentPosterIndex
+    const [currentPosterIndex, setPosterIndex] = useState(0);// 0 here initializes currentPosterIndex
+    const [visible, setVisible] = useState(false);
     useEffect(() => {
         const interval = setInterval(() => {
             setPosterIndex((prevIndex) =>
@@ -41,95 +42,178 @@ export default function loginPage() {
     return (
         <div className="flex min-h-screen overflow-hidden">
             {/* Left Panel - Login Form */}
-            <div className="w-full md:w-[45%] bg-black text-white flex items-center justify-center">
-
-                {/* Login Form */}
-                <div className="flex justify-center px-4 py-20">
-                    {/*Login header, symbol and google OAuth links*/}
-                    <div className="">
-                        <h1 className="text-3xl font-bold p-8">Log In</h1>
-                    </div>
-
-                    <div>
-
-                    </div>
-
-                    <form className="max-w-sm mx-auto">
-                        <div className="mb-5 grid gap-4">
-                            <div className="grid">
-                                <label htmlFor="email"
-                                       className="block mb-2 text-sm font-medium text-white">Your
-                                    email</label>
-                                <input type="email" id="email"
-                                       className="bg-gray-900  border-gray-800 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-300"
-                                       placeholder="Username or E-mail" required/>
-                            </div>
-                            <div >
-                                <label htmlFor="password"
-                                       className="block mb-2 text-sm font-medium text-white">Your
-                                    password</label>
-                                <input type="password" id="password"
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       required/>
-                            </div>
+            <div className="w-full md:w-[50%] bg-black text-white flex items-center justify-center">
+                <div className="flex justify-center px-4 py-15 w-full">
+                    {/* Login Form */}
+                    <div className="flex flex-col justify-center px-4 py-20 gap-4 w-full max-w-md">
+                        {/* Logo and header */}
+                        <div className="flex flex-col items-start">
+                            <svg className="-ml-7" width="130" height="130" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+                                <defs>
+                                    <linearGradient id="meltGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                                        <stop offset="0%" stopColor="#f59e0b" />
+                                        <stop offset="35%" stopColor="#f97316" />
+                                        <stop offset="75%" stopColor="#fb7185" />
+                                    </linearGradient>
+                                </defs>
+                                <path fill="url(#meltGradient)"
+                                      d="M 65 42 L 56 55 L 56 165 L 59 172 L 68 179 L 78 180 L 81 199 L 87 203 L 95 203 L 101 198 L 103 193 L 103 158 L 107 156 L 110 160 L 110 178 L 112 182 L 118 186 L 129 184 L 133 178 L 133 156 L 138 153
+                                        L 140 155 L 141 184 L 148 190 L 160 188 L 164 181 L 164 133 L 184 118 L 187 110 L 184 100 L 178 94 L 150 79 L 143 79 L 135 87 L 133 109 L 128 107 L 127 97 L 122 91 L 113 90 L 107 93 L 104 98
+                                        L 104 121 L 99 123 L 97 121 L 97 103 L 95 99 L 89 95 L 75 96 L 77 103 L 86 103 L 88 105 L 89 124 L 97 132 L 104 132 L 110 128 L 112 124 L 112 101 L 114 99 L 119 101 L 119 108 L 124 116
+                                        L 129 118 L 138 116 L 143 109 L 143 90 L 145 88 L 149 88 L 174 102 L 178 108 L 178 111 L 165 122 L 162 114 L 158 114 L 155 117 L 154 181 L 151 182 L 149 180 L 149 155 L 144 146 L 133 144 L 128 147
+                                        L 125 152 L 125 174 L 123 177 L 118 175 L 118 156 L 116 152 L 111 148 L 102 148 L 97 151 L 94 157 L 94 193 L 90 195 L 87 192 L 87 162 L 81 161 L 78 171 L 71 171 L 65 165 L 65 55 L 71 49 L 78 49 L 126 76 L 131 76 L 133 71 L 81 41 L 71 40 Z"/>
+                            </svg>
+                            <h1 className="text-3xl font-bold m-0">Log In</h1>
                         </div>
-                        <div className="flex items-start mb-5">
-                            <div className="flex items-center h-5">
-                                <input id="remember" type="checkbox" value=""
-                                       className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                                       required/>
-                            </div>
-                            <label htmlFor="remember"
-                                   className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
-                                me</label>
-                        </div>
-                        <button type="submit"
-                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
-                        </button>
-                    </form>
-                </div>
-            </div>
 
-            {/* Fixed Right Panel */}
-            <div className="hidden md:block fixed top-0 right-0 w-[55%] h-screen overflow-hidden z-10">
-                <div className="absolute inset-0 overflow-hidden">
-                    {posters.map((poster, index) => (
-                        <div
-                            key={index}
-                            className={`absolute inset-0 transition-opacity duration-1000 flex flex-col ${poster.bg} ${
-                                index === currentPosterIndex ? "opacity-100" : "opacity-0"
-                            }`}
-                        >
-                            {/* Image container */}
-                            <div className="flex-grow flex items-center justify-center overflow-hidden p-4">
-                                <div className="w-full h-full max-w-full max-h-full flex items-center justify-center">
-                                    <img
-                                        src={poster.src}
-                                        alt={poster.title + " - " + poster.subtitle}
-                                        className="max-w-full max-h-full object-contain rounded-lg"
-                                    />
+                        <form className="w-full">
+                            <div className="mb-5 grid gap-6 w-full">
+                                {/* Email Field */}
+                                <div className="mt-9 grid w-full">
+                                    <div className="relative w-full">
+                                        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                className="h-5 w-5 text-gray-400">
+                                                <path
+                                                    d="M1 4.5 L12 14.5 L23 4.5 M1 4.5 H23V19H1V4.5Z"
+                                                    stroke="#A19F9F"
+                                                    strokeWidth="1.6"
+                                                    strokeLinejoin="round"
+                                                />
+                                            </svg>
+                                        </div>
+                                        <input
+                                            className="flex h-13 w-full rounded-lg border border-gray-500 bg-transparent pl-10 py-2 text-base transition-colors
+                                                       focus-visible:outline-none focus-visible:ring-2 hover:border-gray-300 focus:border-white"
+                                            id="emailOrUsername"
+                                            placeholder="E-mail / Username"
+                                            autoCapitalize="none"
+                                            autoComplete="email"
+                                            autoCorrect="off"
+                                            required
+                                        />
+                                    </div>
+                                </div>
+
+                                {/*Password Field*/}
+                                <div className="grid w-full">
+                                    <div className="relative w-full">
+                                        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                className="h-5 w-5 text-gray-400"
+                                            >
+                                                <path
+                                                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                                                    stroke="#A19F9F"
+                                                    strokeWidth="1.6"
+                                                    strokeLinejoin="round"
+                                                />
+                                            </svg>
+                                        </div>
+                                        <input
+                                            type={visible ? "text" : "password"}
+                                            className="flex h-13 w-full rounded-lg border border-gray-500 bg-transparent pl-10 pr-10 py-2 text-base transition-colors
+                                                       focus-visible:outline-none focus-visible:ring-2 hover:border-gray-300 focus:border-white"
+                                            id="password"
+                                            placeholder="Password"
+                                            autoCapitalize="none"
+                                            autoComplete="current-password"
+                                            required
+                                        />
+                                        {/* Password visibility toggle */}
+                                        <button
+                                            type="button"
+                                            className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 hover:text-gray-300"
+                                            onClick={() => setVisible((prev) => !prev)}
+                                        >
+                                            {
+                                                visible ? (
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                        <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                                        <path
+                                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0zM2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                                                            strokeWidth="2"
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                        />
+                                                    </svg>
+                                                ) : (
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                        {/*<path d="M3 3l18 18" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />*/}
+                                                        <path
+                                                            d="M17.94 17.94A10.07 10.07 0 0112 20c-7 0-11-8-11-8a18.45 18.45 0 015.06-5.94M9.9 4.24A9.12 9.12 0 0112 4c7 0 11 8 11 8a18.5 18.5 0 01-2.16 3.19m-6.72-1.07a3 3 0 11-4.24-4.24M1 1l22 22"
+                                                            strokeWidth="2"
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                        />
+                                                    </svg>
+                                                )
+                                            }
+                                        </button>
+                                    </div>
+                                    <div className="mt-4 flex justify-between items-center mb-2">
+                                        <a
+                                            className="text-md text-gray-400 hover:text-gray-200 transition-colors"
+                                            href="/password-reset"
+                                        >
+                                            Forgot password?
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
 
-                            {/* Indicators */}
-                            <div className="w-full flex justify-center pb-3 pt-1">
-                                {posters.map((_, i) => (
-                                    <div
-                                        key={i}
-                                        className={`w-2 h-2 rounded-full transition-all duration-300 mx-1 ${
-                                            i === currentPosterIndex ? "bg-white w-6" : "bg-white/30"
-                                        }`}
-                                    />
-                                ))}
-                            </div>
-
-                            {/* Gradient overlay */}
-                            <div className="absolute inset-0 pointer-events-none">
-                                <div className="absolute inset-0 bg-gradient-to-t from-black/45 to-transparent" />
-                            </div>
-                        </div>
-                    ))}
+                            <button
+                                type="submit"
+                                className="text-black bg-white hover:bg-gray-300 focus:outline-none font-medium rounded-lg text-base w-full py-3.5 text-center transition-colors">
+                                Log In
+                            </button>
+                        </form>
+                    </div>
                 </div>
+            </div>
+
+            {/* Fixed Right Panel, think of the frame analogy. */}
+            <div className="hidden md:block fixed inset-y-0 right-0 w-[50%] h-screen overflow-hidden z-10"> {/*fixed helps keep it in place*/}
+                {posters.map((poster, index) => (
+                    <div
+                        key={index}
+                        className={`absolute inset-0 transition-opacity duration-1000 flex flex-col ${poster.bg} ${
+                            index === currentPosterIndex ? "opacity-100" : "opacity-0"
+                        }`}
+                    >
+                        {/* Image container */}
+                        <div className="flex-grow flex items-center justify-center overflow-hidden p-8">
+                            <img
+                                src={poster.src}
+                                alt={poster.title + " - " + poster.subtitle}
+                                className="max-w-full max-h-full object-contain rounded-xl"
+                            />
+                        </div>
+
+                        {/* Indicators */}
+                        <div className="w-full flex justify-center pb-3 pt-1">
+                            {posters.map((_, i) => (
+                                <div
+                                    key={i}
+                                    className={`w-2 h-2 rounded-full transition-all duration-300 mx-1 ${
+                                        i === currentPosterIndex ? "bg-white w-6" : "bg-white/30"
+                                    }`}
+                                />
+                            ))}
+                        </div>
+
+                        {/* Gradient overlay */}
+                        <div className="absolute inset-0 pointer-events-none">
+                            <div className="absolute inset-0 bg-gradient-to-t from-black/45 to-transparent" />
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
